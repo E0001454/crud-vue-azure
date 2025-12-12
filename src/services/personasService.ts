ccbc149843042cc17900623baf8e432dcb301c61
theLuazBadak <luis.vasquez@badak.mx>
2025-12-12 09:05:25 -0600

    feat: Servicios actualizados listos para recibir backend en azure

--- src/services/personasService.ts
+++ src/services/personasService.ts
@@ -1,8 +1,12 @@
 import type { Persona } from "@/types/persona";
 import { mockPersonas } from "@/mock/personas";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-const USE_MOCK = import.meta.env.VITE_USE_MOCK === 'true';
+const API_URL = import.meta.env.VITE_API_URL as string;
+
+if(!API_URL && import.meta.env.VITE_USE_MOCK !== 'true'){
+    throw new Error('Vite api url no está definido');
+}
+const USE_MOCK = String(import.meta.env.VITE_USE_MOCK) === 'true';
 
 const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
