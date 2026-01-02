5e32e1aa8ea8e2f2541e0369eb58f2b69bff9850
theLuazBadak <luis.vasquez@badak.mx>
2026-01-02 13:18:45 -0600

    feat: nuevo API del backend

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,5 +1,5 @@
 export async function getSasUrl(filename: string): Promise<{ uploadUrl: string }>{
     const res = await fetch(`https://crud-vue-azure-config-e7ccfgbmfrbghvg6.canadacentral-01.azurewebsites.net/api/httpTrigger2?filename=${encodeURIComponent(filename)}`)
     if(!res.ok) throw new Error('No se obtuvo el SAS')
-    return res.json()
+    return res.json() 
 }
